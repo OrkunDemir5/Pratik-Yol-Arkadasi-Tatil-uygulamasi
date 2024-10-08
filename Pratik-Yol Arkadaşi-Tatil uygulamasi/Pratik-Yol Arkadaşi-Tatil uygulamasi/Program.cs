441cd9983f832c7361e98ca9e520d8080c6c1bc8
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-08 14:56:42 +0300

    Pratik-Yol-Arkadasi-Tatil-uygulamasi

---
+++ Pratik-Yol Arkadaşi-Tatil uygulamasi/Pratik-Yol Arkadaşi-Tatil uygulamasi/Program.cs
@@ -0,0 +1,78 @@
+﻿bool devam;
+do
+{
+    //Lokasyon Seçimi
+    Console.WriteLine("Merhaba, gitmek istediğiniz yeri seçin");
+
+    string secim = "";
+    int toplamTutar = 0;
+    bool lokasyonSecim = false;
+    do
+    {
+        Console.WriteLine("1 - Bodrum (Paket başlangıç fiyatı 4000 TL)\n" +
+        "2 - Marmaris (Paket başlangıç fiyatı 3000 TL)\n3 - Çeşme (Paket başlangıç fiyatı 5000 TL)");
+        secim = Console.ReadLine().ToLower();
+        switch (secim)
+        {
+            case "bodrum":
+                toplamTutar += 4000;
+                lokasyonSecim = true;
+                Console.WriteLine("1. Bodrum Kalesi\n2. Bodrum Çarşısı\n3. Bodrum Antik Tiyatrosu");
+                break;
+            case "marmaris":
+                toplamTutar += 3000;
+                lokasyonSecim = true;
+                Console.WriteLine("1. Marmaris Kalesi ve Müzesi\n2. Turgut Şelalesi\n3. Dalaman Çayı");
+                break;
+            case "çeşme":
+                toplamTutar += 5000;
+                lokasyonSecim = true;
+                Console.WriteLine("1. Alaçatı\n2. Çeşme Marina\n3. Delikli Koy");
+                break;
+            default:
+                Console.WriteLine("Hatalı giriş! Lütfen geçerli bir lokasyon girin: Bodrum, Marmaris, Çeşme.");
+                break;
+        }
+    } while (!lokasyonSecim);
+    //Kişi sayısı
+    Console.WriteLine("Kaç kişi olacaksınız ?");
+    int kisiSayi = int.Parse(Console.ReadLine());
+
+    // Ulaşım seçimi
+    bool ulasimSecildi = false;
+    do
+    {
+        Console.WriteLine("1 - Kara yolu ( Kişi başı ulaşım tutarı gidiş-dönüş 1500 TL )\n" +
+        "2 - Hava yolu ( Kişi başı ulaşım tutarı gidiş-dönüş 4000 TL)\n" +
+        "Lütfen yukarıdaki seçeneklerden bir tanesini seçiniz");
+
+        string ulasimSecim = Console.ReadLine();
+        if (ulasimSecim == "1")
+        {
+            toplamTutar += 1500;
+            ulasimSecildi = true;
+        }
+        else if (ulasimSecim == "2")
+        {
+            toplamTutar += 4000;
+            ulasimSecildi = true;
+        }
+        else
+        {
+            Console.WriteLine("Hatalı girdiniz! Tekrar deneyin");
+        }
+
+    } while (!ulasimSecildi);
+
+    Console.WriteLine($"toplam fiyat: {toplamTutar * kisiSayi}");
+
+    //Başka bir tatil planı ?
+    Console.WriteLine("Başka bir tatil planı nister misin ? (Evet/Hayır)");
+    string cevap = Console.ReadLine().ToLower();
+
+    devam = (cevap == "evet");
+}while (devam);
+
+Console.WriteLine("İyi günler dileriz!");
+
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Pratik-Yol-Arkadasi-Tatil-uygulamasi
+# Tatil Planlama Uygulaması
+Bu C# uygulaması, kullanıcıların 3 günlük bir tatil planlamasına yardımcı olmak için tasarlanmıştır. Uygulama kullanıcıya, tatil lokasyonu, kişi sayısı ve ulaşım türü gibi seçenekler sunar ve seçilen seçeneklere göre toplam tatil maliyetini hesaplar. Ayrıca, kullanıcı isterse başka bir tatil planlayabilir veya uygulamayı sonlandırabilir.
+
+## Özellikler
+ 1- 3 Tatil Lokasyonu: Bodrum, Marmaris, Çeşme.
+ 2- 2 Ulaşım Seçeneği: Kara yolu (1500 TL kişi başı), Hava yolu (4000 TL kişi başı).
+ 3- Kullanıcıdan lokasyon, kişi sayısı ve ulaşım tercihi alınır.
+ 4- Toplam maliyet hesaplanır ve kullanıcıya sunulur.
+ 5- Kullanıcıya başka bir tatil planlamak isteyip istemediği sorulur.
  
